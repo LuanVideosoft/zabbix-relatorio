8e48bc5ed90f2cdb36c28553d8f764f116ba750b
LuanVideosoft <luan.santos@wow.tec.br>
2026-05-28 20:44:01 -0300

    att

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ with col2:
     # Relatório Operacional Totem
 
     <span style='color:gray'>
-    Videosoft • Sistema Operacional Zabbix
+    Videosoft • 
     </span>
     """, unsafe_allow_html=True)
 
